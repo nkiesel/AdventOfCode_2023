558d0daf3340998feff661be6c7a079883cc219c
Norbert Kiesel <nk@iname.com>
2023-12-01 09:15:37 -0800

    further improved Day01::three

--- src/test/kotlin/Day01.kt
+++ src/test/kotlin/Day01.kt
@@ -79,11 +79,9 @@ class Day01 {
             "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5,
             "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
         )
-        val pattern = map.keys.joinToString("|").toRegex()
         fun calibration(line: String): Int {
-            fun IntProgression.digit() = firstNotNullOf { i -> pattern.find(line, i)?.let { map[it.value]!! } }
-            val first = line.indices.digit()
-            val last = line.indices.reversed().digit()
+            val first = map[line.findAnyOf(map.keys)!!.second]!!
+            val last = map[line.findLastAnyOf(map.keys)!!.second]!!
             return first * 10 + last
         }
         return input.sumOf { calibration(it) }
