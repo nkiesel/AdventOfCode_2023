2c58c6cc6f572895b0fb853449f93744d5dd7234
Norbert Kiesel <nk@iname.com>
2023-12-15 23:22:33 -0800

    Day 16

--- src/test/kotlin/Day16.kt
+++ src/test/kotlin/Day16.kt
@@ -18,59 +18,49 @@ class Day16 {
 
     enum class Direction { N, S, E, W }
 
-    data class Beam(val x: Int, val y: Int, val d: Direction)
+    data class Beam(val p: IntPair, val d: Direction)
 
     private fun energized(area: CharArea, start: Beam): Int {
         var beams = setOf(start)
-        val energized = mutableSetOf<Pair<Int, Int>>()
+        val energized = mutableSetOf<IntPair>()
         val seen = mutableSetOf<Beam>()
         do {
             beams = buildSet {
                 beams.filter { seen.add(it) }.forEach { beam ->
                     val d = beam.d
-                    val x = beam.x + when (d) {
-                        E -> 1
-                        W -> -1
-                        else -> 0
-                    }
-                    val y = beam.y + when (d) {
-                        N -> -1
-                        S -> 1
-                        else -> 0
-                    }
-                    if (x in area.xRange && y in area.yRange) {
-                        energized += Pair(x, y)
-                        when (area.get(x, y)) {
-                            '-' -> if (d == N || d == S) {
-                                add(Beam(x, y, E))
-                                add(Beam(x, y, W))
-                            } else {
-                                add(Beam(x, y, d))
-                            }
-
-                            '|' -> if (d == E || d == W) {
-                                add(Beam(x, y, N))
-                                add(Beam(x, y, S))
-                            } else {
-                                add(Beam(x, y, d))
-                            }
-
+                    val p = Pair(
+                        beam.p.first + when (d) {
+                            E -> 1
+                            W -> -1
+                            else -> 0
+                        },
+                        beam.p.second + when (d) {
+                            N -> -1
+                            S -> 1
+                            else -> 0
+                        }
+                    )
+                    if (p in area) {
+                        energized += p
+                        when (area.get(p)) {
                             '\\' -> when (d) {
-                                E -> add(Beam(x, y, S))
-                                W -> add(Beam(x, y, N))
-                                S -> add(Beam(x, y, E))
-                                N -> add(Beam(x, y, W))
+                                E -> listOf(S)
+                                W -> listOf(N)
+                                S -> listOf(E)
+                                N -> listOf(W)
                             }
 
                             '/' -> when (d) {
-                                E -> add(Beam(x, y, N))
-                                W -> add(Beam(x, y, S))
-                                S -> add(Beam(x, y, W))
-                                N -> add(Beam(x, y, E))
+                                E -> listOf(N)
+                                W -> listOf(S)
+                                S -> listOf(W)
+                                N -> listOf(E)
                             }
 
-                            '.' -> add(Beam(x, y, d))
-                        }
+                            '-' -> if (d == N || d == S) listOf(E, W) else listOf(d)
+                            '|' -> if (d == E || d == W) listOf(N, S) else listOf(d)
+                            else -> listOf(d)
+                        }.forEach { add(Beam(p, it)) }
                     }
                 }
             }
@@ -79,7 +69,7 @@ class Day16 {
     }
 
     private fun one(input: List<String>): Int {
-        return energized(CharArea(input), Beam(-1, 0, E))
+        return energized(CharArea(input), Beam(Pair(-1, 0), E))
     }
 
     private fun two(input: List<String>): Int {
@@ -87,10 +77,10 @@ class Day16 {
         val mx = area.xRange.last + 1
         val my = area.yRange.last + 1
         return listOf(
-            area.yRange.maxOf { y -> energized(area, Beam(-1, y, E)) },
-            area.yRange.maxOf { y -> energized(area, Beam(mx, y, W)) },
-            area.xRange.maxOf { x -> energized(area, Beam(x, -1, S)) },
-            area.xRange.maxOf { x -> energized(area, Beam(x, my, N)) },
+            area.yRange.maxOf { y -> energized(area, Beam(Pair(-1, y), E)) },
+            area.yRange.maxOf { y -> energized(area, Beam(Pair(mx, y), W)) },
+            area.xRange.maxOf { x -> energized(area, Beam(Pair(x, -1), S)) },
+            area.xRange.maxOf { x -> energized(area, Beam(Pair(x, my), N)) },
         ).max()
     }
 
