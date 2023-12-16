5dfa9f9cbf0351c7482af37c3d7012f57632b533
Norbert Kiesel <nk@iname.com>
2023-12-15 23:05:21 -0800

    Day 16

--- src/test/kotlin/Day16.kt
+++ src/test/kotlin/Day16.kt
@@ -1,3 +1,4 @@
+import Day16.Direction.*
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -15,51 +16,60 @@ class Day16 {
         ..//.|....
     """.trimIndent().lines()
 
-    data class Beam(val x: Int, val y: Int, val dx: Int, val dy: Int)
+    enum class Direction { N, S, E, W }
 
-    private fun one(area: CharArea, start: Beam): Int {
-        var beams = listOf(start)
+    data class Beam(val x: Int, val y: Int, val d: Direction)
+
+    private fun energized(area: CharArea, start: Beam): Int {
+        var beams = setOf(start)
         val energized = mutableSetOf<Pair<Int, Int>>()
         val seen = mutableSetOf<Beam>()
         do {
-            beams = buildList {
-                for (beam in beams.filter { seen.add(it) }) {
-                    val dx = beam.dx
-                    val dy = beam.dy
-                    val x = beam.x + dx
-                    val y = beam.y + dy
+            beams = buildSet {
+                beams.filter { seen.add(it) }.forEach { beam ->
+                    val d = beam.d
+                    val x = beam.x + when (d) {
+                        E -> 1
+                        W -> -1
+                        else -> 0
+                    }
+                    val y = beam.y + when (d) {
+                        N -> -1
+                        S -> 1
+                        else -> 0
+                    }
                     if (x in area.xRange && y in area.yRange) {
                         energized += Pair(x, y)
                         when (area.get(x, y)) {
-                            '-' -> if (dx == 0) {
-                                add(Beam(x, y, -1, 0))
-                                add(Beam(x, y, 1, 0))
+                            '-' -> if (d == N || d == S) {
+                                add(Beam(x, y, E))
+                                add(Beam(x, y, W))
                             } else {
-                                add(Beam(x, y, dx, dy))
+                                add(Beam(x, y, d))
                             }
 
-                            '|' -> if (dy == 0) {
-                                add(Beam(x, y, 0, -1))
-                                add(Beam(x, y, 0, 1))
+                            '|' -> if (d == E || d == W) {
+                                add(Beam(x, y, N))
+                                add(Beam(x, y, S))
                             } else {
-                                add(Beam(x, y, dx, dy))
+                                add(Beam(x, y, d))
                             }
 
-                            '\\' -> when {
-                                dx == 1 -> add(Beam(x, y, 0, 1))
-                                dx == -1 -> add(Beam(x, y, 0, -1))
-                                dy == 1 -> add(Beam(x, y, 1, 0))
-                                dy == -1 -> add(Beam(x, y, -1, 0))
+                            '\\' -> when (d) {
+                                E -> add(Beam(x, y, S))
+                                W -> add(Beam(x, y, N))
+                                S -> add(Beam(x, y, E))
+                                N -> add(Beam(x, y, W))
                             }
 
-                            '/' -> when {
-                                dx == 1 -> add(Beam(x, y, 0, -1))
-                                dx == -1 -> add(Beam(x, y, 0, 1))
-                                dy == 1 -> add(Beam(x, y, -1, 0))
-                                dy == -1 -> add(Beam(x, y, 1, 0))
+                            '/' -> when (d) {
+                                E -> add(Beam(x, y, N))
+                                W -> add(Beam(x, y, S))
+                                S -> add(Beam(x, y, W))
+                                N -> add(Beam(x, y, E))
                             }
 
-                            '.' -> add(Beam(x, y, dx, dy))
+                            '.' -> add(Beam(x, y, d))
                         }
                     }
                 }
@@ -68,23 +78,26 @@ class Day16 {
         return energized.size
     }
 
+    private fun one(input: List<String>): Int {
+        return energized(CharArea(input), Beam(-1, 0, E))
+    }
+
     private fun two(input: List<String>): Int {
         val area = CharArea(input)
         val mx = area.xRange.last + 1
         val my = area.yRange.last + 1
         return listOf(
-            area.yRange.maxOf { y -> one(area, Beam(-1, y, 1, 0)) },
-            area.yRange.maxOf { y -> one(area, Beam(mx, y, -1, 0)) },
-            area.xRange.maxOf { x -> one(area, Beam(x, -1, 0, 1)) },
-            area.xRange.maxOf { x -> one(area, Beam(x, my, 0, -1)) },
+            area.yRange.maxOf { y -> energized(area, Beam(-1, y, E)) },
+            area.yRange.maxOf { y -> energized(area, Beam(mx, y, W)) },
+            area.xRange.maxOf { x -> energized(area, Beam(x, -1, S)) },
+            area.xRange.maxOf { x -> energized(area, Beam(x, my, N)) },
         ).max()
     }
 
     @Test
     fun testOne(input: List<String>) {
-        val start = Beam(-1, 0, 1, 0)
-        one(CharArea(sample), start) shouldBe 46
-        one(CharArea(input), start) shouldBe 8249
+        one(sample) shouldBe 46
+        one(input) shouldBe 8249
     }
 
     @Test
@@ -93,3 +106,9 @@ class Day16 {
         two(input) shouldBe 8444
     }
 }
+
+/*
+Again pretty simple from an algorithmic point of view.  One question I had was what happens to beams that hit a
+boundary of the area. Then - apart from one +/- error - the only other issue was that I initially did not add
+the "seen" cache, which made the code run forever.
+*/
